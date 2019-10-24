3e95c18367319de9e4dfefb0310d6ca2fe58f66e
FuFuWarrior <52762328+FuFuWarrior@users.noreply.github.com>
2019-10-24 22:59:45 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,137 @@
+// MONGODB PASSWORD: RZ5c3WeouQc3nUnq
+//MONGODB CONNECTION: mongodb+srv://fufu:<password>@cluster0-nwh02.mongodb.net/admin?retryWrites=true&w=majority
+// MONGODB 2 : mongodb://fufu:RZ5c3WeouQc3nUnq@cluster0-shard-00-00-nwh02.mongodb.net:27017,cluster0-shard-00-01-nwh02.mongodb.net:27017,cluster0-shard-00-02-nwh02.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority
+
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+
+const Recipe = require('./models/recipe');
+
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect('mongodb://fufu:RZ5c3WeouQc3nUnq@cluster0-shard-00-00-nwh02.mongodb.net:27017,cluster0-shard-00-01-nwh02.mongodb.net:27017,cluster0-shard-00-02-nwh02.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',{useNewUrlParser: true})
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
+  
+
+/*app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });*/
+
+
+
+app.use(bodyParser.json());
+
+app.post('/api/recipes', (req, res, next) => {
+    const recipe = new Recipe({
+      title: req.body.title,
+      ingredients: req.body.ingredients,
+      instructions: req.body.instructions,
+      difficulty: req.body.difficulty,
+      time: req.body.time
+    });
+    recipe.save().then(
+      () => {
+        res.status(201).json({
+          message: 'Post saved successfully!'
+        });
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  });
+
+app.use('/api/recipes', (req, res, next) => {
+  Recipe.find().then(
+    (recipes) => {
+       res.status(200).json(recipes);
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  });
+
+  app.use('/api/recipes/:id', (req, res, next) => {
+    Recipe.findOne({_id: req.params.id}).then(
+      (recipe) => {
+         res.status(200).json(recipe);
+        }
+      ).catch(
+        (error) => {
+          res.status(400).json({
+            error: error
+          });
+        }
+      );
+    });
+  
+
+  app.put('/api/recipes/:id', (req, res, next) => {
+    const recipe = new Recipe({
+      _id: req.params.id,
+      title: req.body.title,
+      ingredients: req.body.ingredients,
+      instructions: req.body.instructions,
+      difficulty: req.body.difficulty,
+      time: req.body.time
+    });
+    Recipe.updateOne({_id: req.params.id}, recipe).then(
+      () => {
+        res.status(201).json({
+          message: 'Thing updated successfully!'
+        });
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  });
+
+  app.delete('/api/recipes/:id', (req, res, next) => {
+    Recipe.deleteOne({_id: req.params.id}).then(
+      () => {
+        res.status(200).json({
+          message: 'Deleted!'
+        });
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  });
+
+
+
+module.exports = app;
